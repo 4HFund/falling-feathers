6b544ddf19f281f81e71daf6628b8d368038500f
4HFund <sidney.mozingo@gmail.com>
2026-07-10 19:34:25 -0400

    Connect public gallery to live Cloudflare API

--- site-config.js
+++ site-config.js
@@ -1,5 +1,3 @@
 window.FFH_CONFIG = {
-  // Paste the deployed Cloudflare Worker address here after setup.
-  // Example: https://falling-feathers-admin.your-subdomain.workers.dev
-  apiBase: ''
+  apiBase: 'https://falling-feathers-admin.sidney-mozingo.workers.dev'
 };

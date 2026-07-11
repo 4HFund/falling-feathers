58c9df63a117132f71ac0016273e60c87a38b820
4HFund <sidney.mozingo@gmail.com>
2026-07-11 00:11:31 -0400

    Load egg product photo controls

--- site-config.js
+++ site-config.js
@@ -14,6 +14,7 @@ if (/\/admin\.html$/i.test(window.location.pathname)) {
     'admin-vision.js?v=20260710-2',
     'homepage-gallery-control.js?v=20260710-1',
     'egg-inventory-admin.js?v=20260710-1',
+    'egg-product-photos-admin.js?v=20260711-1',
     'flock-images-admin.js?v=20260710-1',
     'about-page-admin.js?v=20260710-1',
     'admin-quick-publish.js?v=20260711-1'

4bff2d4665af3165614ece3ce2ee75010b9b8c49
4HFund <sidney.mozingo@gmail.com>
2026-07-10 21:12:01 -0400

    Load the advanced admin vision suite on the control center

--- site-config.js
+++ site-config.js
@@ -1,3 +1,10 @@
 window.FFH_CONFIG = {
   apiBase: 'https://falling-feathers-admin.sidney-mozingo.workers.dev'
 };
+
+if (/\/admin\.html$/i.test(window.location.pathname)) {
+  const script = document.createElement('script');
+  script.src = 'admin-vision.js';
+  script.defer = true;
+  document.head.appendChild(script);
+}
